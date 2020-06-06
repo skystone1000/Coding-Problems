dfc356338f4be81393ff957b37aca4d087ffb8cf
Aditya Mahajan <34326438+skystone1000@users.noreply.github.com>
2020-06-06 20:37:58 +0530

    5. Random Pick weight

---
+++ Leet Code/June Challange 2020/5. Random Pick with Weight.cpp
@@ -0,0 +1,40 @@
+// 5. Random Pick with Weight
+
+class Solution {
+public:
+    int n;
+    vector<int> sumArr;   
+    int sum =0;
+    Solution(vector<int>& w) {
+        sum=0;
+        for(int i :w){
+            sum +=i;
+            sumArr.push_back(sum);
+        }
+        
+    }
+    
+    int pickIndex() {
+        int ans = rand() % sum ;
+        return binarySearch(ans+1);
+    }
+    
+    int binarySearch(int val){
+        int l=0;
+        int r=sumArr.size() -1;
+        while(l<r){
+            int mid = l + (r-l)/2;
+            if(sumArr[mid]<val)
+                l=mid+1;
+            else
+                r=mid;
+        }
+        return l;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
