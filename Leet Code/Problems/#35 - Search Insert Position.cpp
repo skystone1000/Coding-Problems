afb492630250f571c4cc39bd4037e3dcb1322207
Aditya Mahajan <34326438+skystone1000@users.noreply.github.com>
2020-06-10 19:42:20 +0530

    Leet code 35 - Search Insert Position

---
+++ Leet Code/Problems/#35 - Search Insert Position.cpp
@@ -0,0 +1,26 @@
+// Leetcode #35 - Search Insert Position
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        /*
+        // Method 1 (to find)        
+        auto it = find(nums.begin(),nums.end(),target);
+        if(it != nums.end()){
+            return it - nums.begin();
+        }*/
+        
+        // Method 2 if not present
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] == target){
+                return i;
+            }else if(nums[i] > target){
+                return i;
+            }else{
+                continue;
+            }
+        }
+        return nums.size();
+        
+    }
+};
