dad70445feff8cf8fcbc12f7c491b862f5fbbb41
skystoneLocal <adityaspmahajan@gmail.com>
2020-08-26 22:49:42 +0530

    Adding updated Template

--- template.cpp
+++ template.cpp
@@ -1,4 +1,6 @@
+//
 // 
+
 #include<iostream>
 #include<vector>
 #include<set>
@@ -21,51 +23,11 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    // Save output to a file
-    // freopen ("myfile.txt","w",stdout);
-
     int testCases;
     cin>>testCases;
     for(int t=0;t<testCases;t++){
 
-        
-
-        /*string str;
-        getline(cin, str); 
-        
-        string a;
-        cin>>a;
-        
-        // int to string
-        string tempSumStr = to_string(73846)
-
-        // String to int
-        string str1 = "31337 sdkh"; 
-        int myint1 = stoi(str1);  // output => 31337
-
-        const char *str1 = "31337 sdkj"; 
-        int num1 = atoi(str1); 
-        
-        // To Lower case
-        transform(s.begin(), s.end(), s.begin(), ::tolower);
-
-        // Unique
-        vector<int>::iterator ip;
-        ip = std::unique(v.begin(), v.begin() + 12); 
-        v.resize(std::distance(v.begin(), ip)); 
-
-        // Find 
-        std::vector<int>::iterator it; 
-        it = std::find (vec.begin(), vec.end(), required); 
-        if (it != vec.end()) 
-            std::cout << "Element " << required <<" found at : "<< it - vec.begin(); 
-        else
-            std::cout << "Element not found"; 
-        */
 
     }
-
-    // Save Output to file
-    // fclose (stdout);
     return 0;
 }
