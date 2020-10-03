37b7fdbeca7fc9b084aa0656be049b3f163a57ec
skystoneLocal <adityaspmahajan@gmail.com>
2020-10-03 11:24:24 +0530

    Leetcode Oct Challenge

---
+++ Leet Code/Oct Challenge 2020/01 #0933 Number of Recent Calls.cpp
@@ -0,0 +1,24 @@
+// LeetCode Oct Challenge
+// 01 #0933 Number of Recent Calls
+
+class RecentCounter {
+    deque<int> req;
+public:
+    RecentCounter() {
+        req = {};
+    }
+    
+    int ping(int t) {
+        req.push_back(t);
+        while(req.front() < t-3000){
+            req.pop_front();
+        }
+        return req.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
