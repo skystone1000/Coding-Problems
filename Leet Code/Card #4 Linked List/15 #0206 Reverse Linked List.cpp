f308bbea2d65265cc6ff0e050f3c36b647eb438d
skystoneLocal <adityaspmahajan@gmail.com>
2020-12-01 23:41:29 +0530

    Reverse Linked list LC

--- Leet Code/Card #4 Linked List/15 #0206 Reverse Linked List.cpp
+++ Leet Code/Card #4 Linked List/15 #0206 Reverse Linked List.cpp
@@ -1,3 +1,49 @@
 // Company Amazon
 // 15 #0206 Reverse Linked List
 // 
+
+
+// Stack solution remaining
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution1 {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
+        
+        while(curr!=NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+    }
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        
+        ListNode* reversedListHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return reversedListHead;
+    }
+};
