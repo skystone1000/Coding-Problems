cddd8bfab8ddc5a123a8966dff3bbfbaa95e3818
skystoneLocal <adityaspmahajan@gmail.com>
2021-01-03 22:52:01 +0530

    Array By parity

--- Leet Code/Company - Amazon 2/31 #0905. Sort Array By Parity.cpp
+++ Leet Code/Company - Amazon 2/31 #0905. Sort Array By Parity.cpp
@@ -5,14 +5,11 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& A) {
-        int index = 0;
-        for (int i=0;i<A.size();i++){
+        int even = 0;
+        for(int i=0;i<A.size();i++){
             if(A[i] % 2 == 0){
-                int temp = A[i];
-                A[i] = A[index];
-                A[index] = temp;
-                
-                index++;
+                swap(A[even], A[i]);
+                even++;
             }
         }
         return A;
