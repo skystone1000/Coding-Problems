8da1c468f347541fd17babea34a3d862999c2aaf
Aditya <adityaspmahajan@gmail.com>
2021-05-18 14:46:05 +0530

    LC 609 Find Duplicates in file system

---
+++ Leet Code/May Challenge 2021/Day 18 - #0609. Find Duplicate File in System.cpp
@@ -0,0 +1,38 @@
+// May Challenge 2021 - Day 18
+// #0609. Find Duplicate File in System
+// https://leetcode.com/problems/find-duplicate-file-in-system/
+
+class Solution {
+public:
+    vector<vector<string>> findDuplicate(vector<string>& paths) {
+        vector< vector<string> > ans;
+        unordered_map<string, vector<string> > m;
+        
+        for(string path: paths) {
+            string pt;
+            int i;
+            for(i = 0; i <  path.size() && path[i] != ' '; i++) pt += path[i];
+            pt += '/'; // common part for every file
+            i++; // skip the space
+            
+            while(i < path.size()) {
+                string temp, file;
+                
+                while(i < path.size() && path[i] != '(') 
+                    temp += path[i++];
+                
+                while(i < path.size() && path[i] != ' ') 
+                    file += path[i++];
+                
+                m[file].push_back(pt + temp);
+                ++i;
+            }
+        }
+        
+        for(auto &x: m) {
+            if(x.second.size() > 1) 
+                ans.push_back(x.second);
+        }
+        return ans;
+    }
+};
