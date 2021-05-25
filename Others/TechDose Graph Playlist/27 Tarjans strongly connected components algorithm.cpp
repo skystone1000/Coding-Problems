011f32039be3d42d026b960b60a99841bd455f54
skystoneLocal <adityaspmahajan@gmail.com>
2021-05-25 19:47:44 +0530

    Graphs TechDose Aticulation Points

--- Others/TechDose Graph Playlist/27 Tarjans strongly connected components algorithm.cpp
+++ Others/TechDose Graph Playlist/27 Tarjans strongly connected components algorithm.cpp
@@ -1,14 +1,51 @@
 // GRAPHS PLAYLIST TECHDOSE
 
 // #27 Tarjans strongly connected components algorithm
-// Time Complexity: O(V+E)
+// Time Complexity: O(V+E) (3 Times traversal)
 
 #include<bits/stdc++.h>
 using namespace std;
 
 // Check V (number of vertices)
 #define V 7
-#define pb push_back
+#define pb push_back;
+
+vector<vector<int>> adj;
+
+void DFS(int u, vector<int>& disc, vector<int>& low,
+         stack<int>& mystack, vector<bool>& presentInStack){
+    
+    static int time = 0;
+    disc[u] = low[u] = time;
+    time += 1;
+    mystack.push(u);
+    presentInStack[u] = true;
+
+    for(int v : adj[u]){
+        // If v is not visited
+        if(disc[v] == -1){
+            DFS(v, disc, low, mystack, presentInStack);
+            low[u] = min( low[u] , low[v] );
+        }
+
+        // Differentiate Back-Edge(process) and Cross-Edge(ignore)
+        else if(presentInStack[v])
+            low[u] = min( low[u] , disc[v] );
+    }
+
+    // If u is head node of SSC
+    if(low[u] == disc[u]){
+        cout<<"SSC is: ";
+        while(mystack.top() != u){
+            cout<<mystack.top()<<" ";
+            presentInStack[mystack.top()] = false;
+            mystack.pop();
+        }
+        cout<<mystack.top()<<" ";
+        presentInStack[mystack.top()] = false;
+        mystack.pop();
+    }
+}
 
 void findSCCs_Tarjan(){
     vector<int> disc(V, -1);
@@ -18,7 +55,7 @@ void findSCCs_Tarjan(){
 
     for(int i=0;i<V;i++)
         if(disc[i] == -1)
-            DFS()
+            DFS(i, disc, low, mystack, presentInStack);
 }
 
 int main(){
