8983f9bab05787d55bdce98c11e8b53d8766a5b7
Aditya <adityaspmahajan@gmail.com>
2021-07-30 21:38:39 +0530

    Graphs Techdose

---
+++ Others/TechDose Graph Playlist/01 Types of Graph.cpp
@@ -0,0 +1,22 @@
+/*
+01 Types of Graph
+
+    1) Depending on direction
+        a) Undirected Graph
+        b) Directed Graph
+    2) Depending on cycle (Only in directed)
+        a) Cyclic Graph
+        b) Acyclic Graph
+    3) Labelling
+        a) Unlabelled Graph
+        b) Vertex Labbeled Graph
+        c) Edge Labbeled Graph
+    4) Weights
+        a) Weighted Graph
+        b) Unweighted Graph
+    5) Sparcity
+        a) Sparse Graph
+        b) Dense Graph
+
+    Fully connected graph -> Edge from every node to every other node
+*/
---
+++ Others/TechDose Graph Playlist/02 Representation of Graphs.cpp
@@ -0,0 +1,25 @@
+/*
+
+1) Undirected Graph
+    a) Adjacency Matrix
+    b) Adjacency List
+
+
+2) Directed Graph
+    a) Adjacency Matrix
+    b) Adjacency List
+
+
+Pros and Cons
+    a) Adjacency Matrix
+        - Easy to represent and Handle
+        - Consumes a lot of sapce (Not fit for sparse graph)
+        
+    b) Adjacency List
+        - Not easy to handle
+        - Needs linked list and pointer manipulations
+        - Takes Low space (Fit for sparse graph)
+
+Note: Adjacency Matrix is good for dense graph due to low space required 
+      as compared to the Adjacency list due to pointer overheads
+*/
