5b53bfb6d18c1afaf2bb98e697bb3125e9962183
Aditya <adityaspmahajan@gmail.com>
2021-09-09 11:23:17 +0530

    Moved to LC repo Linked List

--- Leet Code/Card #4 Linked List/10 #0707 Design Linked List.cpp
+++
@@ -1,3 +0,0 @@
-// Linked List 10
-// #0707 Design Linked List
-// Similar to first but we have to design doubly linked list
--- Leet Code/Card #4 Linked List/13 #0430 Flatten a Multilevel Doubly Linked List.cpp
+++
@@ -1,2 +0,0 @@
-// Linked List 13
-// 13 #0430 Flatten a Multilevel Doubly Linked List
