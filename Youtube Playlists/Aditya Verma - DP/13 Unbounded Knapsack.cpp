50bd6845e6996a93a95aac9b14ddc69e57fb205e
Aditya <adityaspmahajan@gmail.com>
2022-04-24 17:34:41 +0530

    Unbounded Knapsack

---
+++ Youtube Playlists/Aditya Verma - DP/13 Unbounded Knapsack.cpp
@@ -0,0 +1,20 @@
+// Aditya Verma - DP Playlist 13 Unbounded Knapsack
+/*
+Knapsack Types
+    1) Fractional Knapsack => Solve with greedy method
+    2) 0/1 Knapsack
+    3) Unbounded Knapsack
+        a) Rod Cutting
+        b) Coin Change 1
+        c) Coin Chnage 2
+        d) Maximum ribbon Cut
+
+Differences between 0/1 knapsack and Unbounded knapsack
+    - Multiple occurences of same item are allowed in unbounded which is not in 0/1 knapsack
+        which is why we may come to an element again once traversed in unbounded but we do not
+        consider the element again in 0/1 Knapsack
+         
+
+
+*/
+
