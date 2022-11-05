cb074cc1796a7c7def7fbba15cc14378d7d88b01
Aditya <adityaspmahajan@gmail.com>
2022-11-05 23:30:08 +0530

    RSL Questions

--- Others/RSL Round 2/4.cpp
+++ Others/RSL Round 2/4.cpp
@@ -1,5 +1,6 @@
 /*
-A-4: ​Write a function that takes an array of integers as input and prints the second-maximum difference between any two elements from an array.
+A-4: ​Write a function that takes an array of integers as input and prints the second-maximum 
+difference between any two elements from an array.
 Example:int arr[]={14, 12, 70, 15, 95, 65, 22, 30};
 First max-difference = 95-12=83 
 Second max-difference = 95 -14 = 81 
